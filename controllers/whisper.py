d550a8e2cc6917b6b4dc3c4e8d11af5350145f0f
Rahul Raina <rahulr0609@hotmail.com>
2023-03-05 15:47:17 -0800

    Testing yt captions dl

--- controllers/whisper.py
+++ controllers/whisper.py
@@ -1,6 +1,16 @@
 import openai
+from pytube import YouTube
 import os
 
-def get_transcript(audio):
-    audio_file= open("/path/to/file/audio.mp3", "rb")
+def get_transcript():
+    audio_file= open("/Users/rraina/Documents/pet_projects/chatgpt-hackathon/youtube-ai-summarizer/audio-samples/lex_ai_aaron_smith_levin_short.mp3", "rb")
+    print("Opening file", audio_file)
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print('--- transcript', transcript)
+
+if __name__ == '__main__':
+    # get_transcript()
+    yt = YouTube('https://www.youtube.com/watch?v=eKhVnsZ2xY4')
+    yt.captions['a.en'].download(title='test', srt=False)
+    
+
