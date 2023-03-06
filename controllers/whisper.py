4062d8257f5ddeb2662548bb1dc45b6fe0de4dad
Parthi <parthibanloganathan@users.noreply.github.com>
2023-03-05 16:22:19 -0800

    add chunking

--- controllers/whisper.py
+++ controllers/whisper.py
@@ -1,16 +1,38 @@
-import openai
 from pytube import YouTube
-import os
+from pydub import AudioSegment
+import openai
+import os                                                                                                                                                                                                          
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv(Path(".env"))
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+audio_file_path = # parth here to file
+audio_file= open(audio_file_path, "rb")
+
+# audio = AudioSegment.from_mp3(audio_file_path)
+
+# def chunk_audio():
+#     CHUNK_LENGTH = 10 * 60 * 1000 # 10 mins
+#     total_segments = (len(audio) // CHUNK_LENGTH) + 1
+
+#     for i in range(total_segments):
+#         print(i)
+#         start = i * CHUNK_LENGTH
+#         end = start + CHUNK_LENGTH
+#         segment = audio[start:end]
+#         segment.export(f"segment_{i+1}.mp3", format="mp3")
 
 def get_transcript():
-    audio_file= open("/Users/rraina/Documents/pet_projects/chatgpt-hackathon/youtube-ai-summarizer/audio-samples/lex_ai_aaron_smith_levin_short.mp3", "rb")
     print("Opening file", audio_file)
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     print('--- transcript', transcript)
 
 if __name__ == '__main__':
-    # get_transcript()
-    yt = YouTube('https://www.youtube.com/watch?v=eKhVnsZ2xY4')
-    yt.captions['a.en'].download(title='test', srt=False)
+    get_transcript()
+    # yt = YouTube('https://www.youtube.com/watch?v=eKhVnsZ2xY4')
+    # yt.captions['a.en'].download(title='test', srt=True)
+    # chunk_audio()
     
 
